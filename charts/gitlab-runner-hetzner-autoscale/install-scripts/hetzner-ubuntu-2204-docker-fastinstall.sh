2d7238340fc94c8e2abac994803f15c752960d96
David Hiendl <david.hiendl@dhswt.de>
2022-09-23 22:57:47 +0200

    fixed missing apt-get update in install script

--- charts/gitlab-runner-hetzner-autoscale/install-scripts/hetzner-ubuntu-2204-docker-fastinstall.sh
+++ charts/gitlab-runner-hetzner-autoscale/install-scripts/hetzner-ubuntu-2204-docker-fastinstall.sh
@@ -16,6 +16,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+apt-get update
+
 apt-get install -y \
     docker-ce \
     docker-ce-cli \
